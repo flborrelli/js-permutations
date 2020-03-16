3be68018cfcc478362f365f04ab580134c6da421
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-03-15 22:26:02 -0300

    Implemented another solution

--- permutation-2.js
+++ permutation-2.js
@@ -15,15 +15,16 @@ const allPermutation = (str) => {
     //If indexOf each letter for equals to i, it will keep going, if it would be different it will skip to the next letter in the loop
     if (str.indexOf(eachLetter) !== i)
     continue
-    const remainingLetters = str.slice(0, i) + str.slice(i + 1, str.length);
+    const remainingLetters = str.slice(0, i) + str.slice(i + 1);
     result.push(eachLetter)
-    for (let permutation of allPermutation(remainingLetters)){
-      result.push(eachLetter + permutation) }
+    const permutation = allPermutation(remainingLetters);
+    for (let j = 0; j < permutation.length; j += 1) {
+      result.push(eachLetter + permutation[j]);
+  }
   }
   return result.sort((a, b) => {
     if (a.length < b.length) return -1;
     if (a.length > b.length) return 1;
     return 0;
   });
-  }
-
+}
